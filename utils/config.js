40199e8e22d06afed1a65b5898f37c965a2e9580
youxiubiji <youxiubiji@gmail.com>
2022-04-23 20:30:05 +0800

    部署

--- utils/config.js
+++ utils/config.js
@@ -1,14 +1,20 @@
 //token加密
 const token = {
-  secret: "abcdef",
-  expiresIn: 60 * 60, //以秒表示或描述时间跨度zeit / ms的字符串。如60，"2 days"，"10h"，"7d"，Expiration time，过期时间
+  secret: "wishroot",
+  expiresIn: 24 * 60 * 60, //以秒表示或描述时间跨度zeit / ms的字符串。如60，"2 days"，"10h"，"7d"，Expiration time，过期时间
 };
 //数据库信息
+// const Databases = {
+//   host: "1.117.31.97",
+//   name: "countdown",
+//   user: "andy",
+//   password: "123456",
+// };
 const Databases = {
-  host: "1.117.31.97",
-  name: "countdown",
-  user: "andy",
-  password: "123456",
+  host: "localhost",
+  name: "wish",
+  user: "wish",
+  password: "ZAF2XMHT6DY2Wwjk",
 };
 //小程序信息
 const miniprogram = {

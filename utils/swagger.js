0ebb0d414aab83ca6a85a37fc18a1672ce22b5bc
youxiubiji <youxiubiji@gmail.com>
2022-04-24 22:40:11 +0800

    修改swegger路径

--- utils/swagger.js
+++ utils/swagger.js
@@ -22,7 +22,7 @@ const swaggerDefinition = {
             bearerAuth: [],
         },
     ],
-    host: 'localhost:3000', // 想着改这里，如果不修改，那么接口文档访问地址为：localhost:3000/swagger
+    host: 'https://wish.youxiubiji.com', // 想着改这里，如果不修改，那么接口文档访问地址为：localhost:3000/swagger
     // 根路由
     basePath: '/', // Base path (optional)
     schemes: ['http', 'https'],
